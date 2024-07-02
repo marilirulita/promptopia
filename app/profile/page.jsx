b0e5523e7061246a4ad33906c673dfb74aed668a
mar bautista <marilirulita@gmail.com>
2024-07-02 14:53:25 -0700

    add delete and update end api points

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -6,15 +6,36 @@ import { useRouter } from "next/navigation";
 
 import Profile from "@components/Profile";
 
-const page = () => {
+const MyProfile = () => {
+  const {data: session} = useSession();
+  const [posts, setPosts] = useState([]);
+  const router = useRouter();
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await response.json();
+      setPosts(data);
+    }
+
+    if (session?.user.id) fetchPosts();
+  }, []);
+
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`)
+  }
+
+  const handleDelete = async(post) => {
+
+  }
   return (
     <Profile
     name="My"
     desc="Welcome to your cuztomizer profile page"
-    data={[]}
-    handleEdit={() => {}}
-    handleDelete={() => {}} />
+    data={posts}
+    handleEdit={handleEdit}
+    handleDelete={handleDelete} />
   )
 }
 
-export default page
+export default MyProfile;
